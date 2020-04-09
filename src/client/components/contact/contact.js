82b19e02eafc5bf08bda4e59431fc57dec5599f8
rayna williams <Raynawilliams123@gmail.com>
2020-04-08 20:35:58 -0600

    added my profiles for all my different platforms on my contact pg

--- src/client/components/contact/contact.js
+++ src/client/components/contact/contact.js
@@ -1,61 +1,82 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './contact.scss';
 import axios from 'axios';
 import Spinner from '../spinner/spinner';
+import { CardText, CardBody, Card, CardLink } from 'reactstrap';
 
 export default function Contact() {
-  document.title = 'Contact';
-  const [spinner, setSpinner] = useState(false);
-  const [state, setState] = useState({message: '', status: true});
-  async function sendEmail(e){
-	e.preventDefault();
-	if(localStorage.getItem('dateOfSend')){
-		if(Date.now() - localStorage.getItem('dateOfSend') < 1200000){
-			setState({message: `Fail, Try in ${Math.ceil((1200000 / 60000) - (Date.now() - +localStorage.getItem('dateOfSend'))/60000)} min `, status: false});
-			return;
+	document.title = 'Contact';
+	const [spinner, setSpinner] = useState(false);
+	const [state, setState] = useState({ message: '', status: true });
+	async function sendEmail(e) {
+		e.preventDefault();
+		if (localStorage.getItem('dateOfSend')) {
+			if (Date.now() - localStorage.getItem('dateOfSend') < 1200000) {
+				setState({ message: `Fail, Try in ${Math.ceil((1200000 / 60000) - (Date.now() - +localStorage.getItem('dateOfSend')) / 60000)} min `, status: false });
+				return;
+			}
+		}
+		setSpinner(true);
+		try {
+			const result = await axios.post('raynawilliams123@gmail.com', {
+				text: e.target.emailText.value,
+				them: e.target.emailSubject.value
+			});
+			if (result.status === 200) {
+				window.localStorage.setItem('dateOfSend', Date.now());
+				setState({ ...state, message: 'Success, Thank you', status: true });
+			}
+			setSpinner(false);
+		} catch (err) {
+			// setState({message: err.response.data.message, status: false});
+			setSpinner(false);
 		}
 	}
-	setSpinner(true);
-	try{
-		const result = await axios.post('raynawilliams123@gmail.com', {
-			text: e.target.emailText.value,
-			them: e.target.emailSubject.value
-		});
-		if(result.status === 200){
-			window.localStorage.setItem('dateOfSend', Date.now());
-			setState({...state, message: 'Success, Thank you', status: true});
-		}
-		setSpinner(false);
-	}catch(err){
-		// setState({message: err.response.data.message, status: false});
-		setSpinner(false);
-	}
-  }
 
-	return(
+	return (
+
+
 		<div className='contact'>
-			{spinner ? <Spinner/> : ''}
+			{spinner ? <Spinner /> : ''}
 			<div className="contact-image"></div>
 			<div className='contact-form'>
-				<form onSubmit = {sendEmail}>
-				<label>Enter your theme</label>
-				<br></br>
-				<input required type='text' name="emailSubject" className="form-control z-depth-1" rows="3" placeholder="Write your them"></input>
-				<br></br>
-				<label>Enter your message</label>
-				<br></br>
-				<textarea type='textarea' name="emailText" className="form-control z-depth-1" rows="3" placeholder="Write your message"></textarea>
-				<br></br>
-				<input className='submitButton' type="submit" color='info' value='Send'></input>
-				<br></br>
-				<br></br>
-				<h2 style={{color: state.status ? 'green' : 'red', fontStyle: 'italic', textAlign: 'center'}}>{state.message}</h2>
-				<div className="underline"></div>
+				<form onSubmit={sendEmail}>
+					<label>Enter your theme</label>
+					<br></br>
+					<input required type='text' name="emailSubject" className="form-control z-depth-1" rows="3" placeholder="Write your them"></input>
+					<br></br>
+					<label>Enter your message</label>
+					<br></br>
+					<textarea type='textarea' name="emailText" className="form-control z-depth-1" rows="3" placeholder="Write your message"></textarea>
+					<br></br>
+					<input className='submitButton' type="submit" color='info' value='Send'></input>
+					<br></br>
+					<br></br>
+					<h2 style={{ color: state.status ? 'green' : 'red', fontStyle: 'italic', textAlign: 'center' }}>{state.message}</h2>
+					<div className="underline"></div>
 
-				
-			
+
+
 				</form>
 			</div>
+				<div>
+					<Card>
+						<CardBody>
+
+							<CardLink href="https://github.com/raynawilliams1993">
+								GitHub Profile
+						</CardLink>
+							<CardLink href="https://www.linkedin.com/in/rayna-williams-225965191/">
+								LinkedIn Profile
+						</CardLink>
+							<CardLink href="https://docs.google.com/document/d/1VGxJrZkq4Ok5W4felziz_abvuS-3lW55m7eYnVMsTI0/edit?usp=sharing ">
+								Resume
+						</CardLink>
+						</CardBody>
+						
+				
+					</Card>
+				</div>
 		</div>
 	)
 }
--- src/client/components/landingPage/landingPage.js
+++ src/client/components/landingPage/landingPage.js
@@ -3,21 +3,22 @@ import { Provider, Heading, Subhead } from 'rebass'
 import {
   Hero, CallToAction, ScrollDownIndicator
 } from 'react-landing-page'
- 
+
 const LandingPage = props => (
   <Provider>
     <Hero
       color="white"
-    //   bg="white"
-       
-      backgroundImage="https://images.wallpaperscraft.com/image/paint_stains_mixing_140283_1280x720.jpg"
+      //   bg="white"
+
+      backgroundImage="https://images.wallpaperscraft.com/image/earth_moon_space_galaxy_118437_1280x720.jpg"
     >
-        <Heading>Rayna K. Williams</Heading>
-        <Subhead>Email: raynawilliams123@gmail.com </Subhead>
-        <Subhead> GitHub: https://github.com/raynawilliams1993 </Subhead>
-        <Subhead>LinkedIn: https://www.linkedin.com/in/rayna-williams-225965191/ </Subhead>
-        <Subhead> Resume: https://docs.google.com/document/d/1VGxJrZkq4Ok5W4felziz_abvuS-3lW55m7eYnVMsTI0/edit?usp=sharing </Subhead>
-        
+      <Heading>Rayna K. Williams</Heading>
+
+      <Subhead>Email: raynawilliams123@gmail.com </Subhead>
+
+
+
+
     </Hero>
   </Provider>
 )
