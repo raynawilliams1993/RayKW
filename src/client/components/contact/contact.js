bec0a4e3ac963c4c11a03a1e534e300babab88fa
rayna williams <Raynawilliams123@gmail.com>
2020-04-09 18:57:15 -0600

    trying to style how i want, decided to nix the form for now

--- src/client/components/contact/contact.js
+++ src/client/components/contact/contact.js
@@ -1,64 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './contact.scss';
-import axios from 'axios';
-import Spinner from '../spinner/spinner';
-import { CardText, CardBody, Card, CardLink, CardTitle } from 'reactstrap';
+// import axios from 'axios';
+// import Spinner from '../spinner/spinner';
+import { CardLink, CardBody, Card, CardTitle } from 'reactstrap';
 
-export default function Contact() {
-	document.title = 'Contact';
-	const [spinner, setSpinner] = useState(false);
-	const [state, setState] = useState({ message: '', status: true });
-	async function sendEmail(e) {
-		e.preventDefault();
-		if (localStorage.getItem('dateOfSend')) {
-			if (Date.now() - localStorage.getItem('dateOfSend') < 1200000) {
-				setState({ message: `Fail, Try in ${Math.ceil((1200000 / 60000) - (Date.now() - +localStorage.getItem('dateOfSend')) / 60000)} min `, status: false });
-				return;
-			}
-		}
-		setSpinner(true);
-		try {
-			const result = await axios.post('raynawilliams123@gmail.com', {
-				text: e.target.emailText.value,
-				them: e.target.emailSubject.value
-			});
-			if (result.status === 200) {
-				window.localStorage.setItem('dateOfSend', Date.now());
-				setState({ ...state, message: 'Success, Thank you', status: true });
-			}
-			setSpinner(false);
-		} catch (err) {
-			// setState({message: err.response.data.message, status: false});
-			setSpinner(false);
-		}
-	}
+// need to improve on this im running into an issue, gonna ask a homie**
+// this.state = { value: '' };
+
+// this.handleChange = this.handleChange.bind(this);
+// this.handleSubmit = this.handleSubmit.bind(this);
+
+
+// handleChange(event) {
+// 	this.setState({ value: event.target.value });
+// }
+
+// handleSubmit(event) {
+// 	alert('A name was submitted: ' + this.state.value);
+// 	event.preventDefault();
+// }
+
+function Contact() {
 
 	return (
+		<div>
+			<div class="contact-image"></div>
 
-
-		<div className='contact'>
-			{spinner ? <Spinner /> : ''}
-			<div className="contact-image"></div>
-			<div className='contact-form'>
-				<form onSubmit={sendEmail}>
-					<label>Enter your theme</label>
-					<br></br>
-					<input required type='text' name="emailSubject" className="form-control z-depth-1" rows="3" placeholder="Write your them"></input>
-					<br></br>
-					<label>Enter your message</label>
-					<br></br>
-					<textarea type='textarea' name="emailText" className="form-control z-depth-1" rows="3" placeholder="Write your message"></textarea>
-					<br></br>
-					<input className='submitButton' type="submit" color='info' value='Send'></input>
-					<br></br>
-					<br></br>
-					<h2 style={{ color: state.status ? 'green' : 'red', fontStyle: 'italic', textAlign: 'center' }}>{state.message}</h2>
-					<div className="underline"></div>
-
-
-
-				</form>
-			</div>
+			<div class="underline"></div>
 			<div class="links">
 				<Card>
 					<CardBody>
@@ -67,19 +35,19 @@ export default function Contact() {
 							<li>
 								<CardLink href="https://github.com/raynawilliams1993">
 									GitHub
-						</CardLink>
+									</CardLink>
 
 							</li>
 							<li>
 								<CardLink href="https://www.linkedin.com/in/rayna-williams-225965191/">
 									LinkedIn
-						</CardLink>
+									</CardLink>
 
 							</li>
 							<li>
 								<CardLink href="https://docs.google.com/document/d/1VGxJrZkq4Ok5W4felziz_abvuS-3lW55m7eYnVMsTI0/edit?usp=sharing ">
 									Resume
-						</CardLink>
+									</CardLink>
 
 
 							</li>
@@ -92,5 +60,12 @@ export default function Contact() {
 				</Card>
 			</div>
 		</div>
-	)
+
+	);
 }
+
+export default Contact;
+
+
+
+
--- src/client/components/pages/about.js
+++ src/client/components/pages/about.js
@@ -27,9 +27,7 @@ function About() {
             <p>
               Hello, my name is Rayna Williams, I am twenty-seven years old and have lived in Colorado my entire life. I am very creative, passionate and strive to live my life to the fullest. I enjoy the great outdoors, especially golf because not only am I good at it, but I know that I am also challenging myself physically and mentally as well when playing. Growing up in Denver I have developed and matured, expanding my knowledge just as the city continues to expand its limits. I enjoy the music scene and culture this city has to offer. To sum it up, I am extremely excited to enter Denver's ever-expanding tech world!
             </p>
-            {/* <p>
-              I graduated from Denver University's full-time Coding Boot Camp and received a Certificate in Full Stack Web Development. I enjoyed the fast pace and challenging material of the class. The self-taught learning and research helped me build my desire to create incredible apps and websites in my future careers. The reason I am here today trying to pursue a career as a Junior Web Developer is because my grandfather was a coder during his career working for banks across Colorado and has inspired me to join the family trade. I look up to him so much; he is the reason why I have become a coder and even the reason behind my passion for golf too.
-            </p> */}
+            
 
           </Col>
         </Row>
